f823d2c7ad029c55ab86382b444e49244d4c29dc
jjsub <jsamesub@gmail.com>
2015-12-05 14:20:59 -0600

    route added and url module

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 var http = require("http");
+var url =  require("url");
 
-function start(){
+function start(route){
   function onRequest(request, response){
-    console.log("Request received.");
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request" + pathname + " received.");
+    
+    route(pathname);
+    
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello Juan my Friend : )");
     response.end();
   }
+  
   http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
 }
-
-
-console.log("Server has started.");
 
 exports.start = start;
 
