dec2ec44c40c5c8996007d2fb4e46ea13a511530
jjsub <jsamesub@gmail.com>
2015-12-07 18:33:58 -0600

    the blocking way ( not good for the event-loop )

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,22 @@
+// start() contains a blocking operation. We already talked about Node’s
+//  execution model - expensive operations are ok, but we must take care to not block the Node.js 
+//  process with them. Instead, whenever expensive operations must be executed, these must be put in 
+//  the background, and their events must be handled by the event loop.
+// And we will now see why the way we constructed the “request handler
+//  response handling” in our application doesn’t allow us to make 
+//  proper use of non-blocking operations.
+
+
 function start() {
   console.log("*** Request handler 'start' was called.");
+  
+  function sleep(milliSeconds) {
+    var startTime = new Date().getTime();
+    while (
+      new Date().getTime() < startTime + milliSeconds
+      );
+  }
+  sleep(10000);
   return "안녕 Hello start";
 }
 
