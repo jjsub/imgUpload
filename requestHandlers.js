b0946f9ef9fd614bb5edeb146be5f76ff83fbc86
jjsub <jsamesub@gmail.com>
2015-12-09 09:39:32 -0600

    Non-bloking func and routes

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,15 +1,28 @@
-// start() contains a blocking operation. We already talked about Node’s
-//  execution model - expensive operations are ok, but we must take care to not block the Node.js 
-//  process with them. Instead, whenever expensive operations must be executed, these must be put in 
-//  the background, and their events must be handled by the event loop.
-// And we will now see why the way we constructed the “request handler
-//  response handling” in our application doesn’t allow us to make 
-//  proper use of non-blocking operations.
+var exec = require("child_process").exec;
+
+function start(response) {
+console.log("Request handler 'start' was called.");
+exec("ls -lah", function (error, stdout, stderr) { response.writeHead(200, {"Content-Type": "text/plain"}); response.write(stdout);
+response.end();
+}); }
 
 
-function start() {
-  console.log("*** Request handler 'start' was called.");
+
+function upload(response) {
+  console.log("Request handler 'uplaod' was called.");
   
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello updload");
+  response.end();
+}
+
+exports.start  = start;
+exports.upload = upload;
+
+
+
+/* **********
+
   function sleep(milliSeconds) {
     var startTime = new Date().getTime();
     while (
@@ -17,13 +30,5 @@ function start() {
       );
   }
   sleep(10000);
-  return "안녕 Hello start";
-}
 
-function upload() {
-  console.log("Request handler 'uplaod' was called.");
-  return "안녕 Hello uplaod";
-}
-
-exports.start  = start;
-exports.upload = upload;
+************ */
--- server.js
+++ server.js
@@ -1,23 +1,14 @@
-var http = require("http");
-var url =  require("url");
 
-function start(route, handle){
-  function onRequest(request, response){
-    var pathname = url.parse(request.url).pathname;
-    var content  = route(handle, pathname);
 
-    console.log("Request" + pathname + " received.");
-    
-    route(handle, pathname);
-    
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(content);
-    response.end();
+var http = require("http"); var url = require("url");
+function start(route, handle) {
+function onRequest(request, response) {
+var pathname = url.parse(request.url).pathname; 
+console.log("Request for " + pathname + " received.");
+    route(handle, pathname, response);
   }
-  
   http.createServer(onRequest).listen(8888);
   console.log("Server has started.");
 }
-
 exports.start = start;
 
